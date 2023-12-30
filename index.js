b98e445caeaba4c55473a01725e0c0693969920a
Alowna <alonum2004@gmail.com>
2023-12-30 20:10:50 -0300

    Removido Anotações de index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/*Se vitórias for menor do que 10 = Ferro<br>
-Se vitórias for entre 11 e 20 = Bronze<br>
-Se vitórias for entre 21 e 50 = Prata<br>
-Se vitórias for entre 51 e 80 = Ouro<br>
-Se vitórias for entre 81 e 90 = Diamante<br>
-Se vitórias for entre 91 e 100 = Lendário<br>
-Se vitórias for maior ou igual a 101 = Imortal<br>
-(vitorias - derrotas)
-*/
-
 let playerVictories = 45
 let playerDefeats = 30
 let saldoPlayer = saldoGet(playerVictories, playerDefeats)
